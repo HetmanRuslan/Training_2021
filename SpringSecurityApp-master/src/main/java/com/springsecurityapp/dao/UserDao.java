1f7987f2a30fe7bb6a63cf4f17791c3b49ed2d41
Technologyk <58766721+Ruslan10-h@users.noreply.github.com>
2022-02-02 15:18:31 +0200

    Add files via upload

---
+++ SpringSecurityApp-master/src/main/java/com/springsecurityapp/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.springsecurityapp.dao;
+
+import com.springsecurityapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
