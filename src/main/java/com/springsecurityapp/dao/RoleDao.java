fe1c3efaeba91d97e3aed37242ece2b2895acec7
Technologyk <58766721+Ruslan10-h@users.noreply.github.com>
2022-02-02 15:11:36 +0200

    Delete src/main directory

--- src/main/java/com/springsecurityapp/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.springsecurityapp.dao;
-
-import com.springsecurityapp.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/springsecurityapp/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.springsecurityapp.dao;
-
-import com.springsecurityapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
