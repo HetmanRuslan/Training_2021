836e727020d91d213ee86089e0723e7b7c0edfa5
Technologyk <58766721+Ruslan10-h@users.noreply.github.com>
2022-02-04 15:23:20 +0200

    Add files via upload

--- demo/src/main/java/com/example/demo/user/UserRepository.java
+++ demo/src/main/java/com/example/demo/user/UserRepository.java
@@ -10,6 +10,16 @@ import java.util.Optional;
 @Repository
 public interface UserRepository
         extends JpaRepository<User,Long> {
+
 @Query("SELECT u FROM User u where u.email=?1")
     Optional<User>findUserByEmail(String email);
+
+    @Query("" +
+            "SELECT CASE WHEN COUNT(u) > 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM User u " +
+            "WHERE u.email = ?1"
+    )
+    Boolean selectExistsEmail(String email);
+
 }
